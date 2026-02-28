36a72594ca65b67567a4205f6ffb5e4e306b31b3
Delfia Ghosh <fila.fernandes.11@gmail.com>
2026-02-28 17:28:59 +0530

    feat(home): added more skills & integrated responsiveness of projects

--- src/components/ProjectList/ProjectList.vue
+++ src/components/ProjectList/ProjectList.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-  import { ref } from 'vue'
-  import { projectsData, projectsPreview } from './projects.ts'
+  import { ref } from 'vue';
+  import { projectsData, projectsPreview } from './projects.ts';
 
-  import './project-list.css'
+  import './project-list.css';
 
   const props = defineProps<{
     isPreview: boolean
@@ -14,13 +14,11 @@
 <template>
   <li v-for="project in projects">
     <a target="_blank" class='card' v-bind:href="project.url">
-      <div class='title'>
-        <p style="font-weight: 600; margin-bottom: 10px;">{{ project.title }}</p>
-        <p v-for="desc in project.description" style="font-style: italic; font-size: medium; padding: 5px 0px;">
-          {{ desc }}
-        </p>
+      <div class='project-info'>
+        <p class="project-title">{{ project.title }}</p>
+        <p class="project-description" v-for="desc in project.description" :key="desc">{{ desc }}</p>
       </div>
-      <img alt="{{ project.title }}" v-bind:src="project.image" loading='lazy' decoding='async' />
+      <img class="project-image" alt="{{ project.title }}" v-bind:src="project.image" loading='lazy' decoding='async' />
     </a>
   </li>
 </template>
